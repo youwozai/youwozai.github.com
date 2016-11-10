3ede15a3d6a801235aab5150e17738cee55ff776
youwozai <18037553413@163.com>
2016-11-10 18:06:42 +0800

    Add files via upload

---
+++ javascripts/move.js
@@ -0,0 +1,17 @@
+﻿//切换到相关页
+function gopage(n) {
+    var tag = document.getElementById("tabmenu").getElementsByTagName("a");
+    var taglength = tag.length; 
+    for (i = 1; i <= taglength; i++) {
+        if (i == n) {
+            document.getElementById("m" + i).className = "on";
+            document.getElementById("c" + i).style.display = '';
+            document.getElementById("m" + i).style.backgroundColor = "gray";
+        } else {
+            document.getElementById("m" + i).style.backgroundColor = "";
+            document.getElementById("m" + i).className = "";
+            document.getElementById("c" + i).style.display = 'none';
+
+        }
+    }
+}
