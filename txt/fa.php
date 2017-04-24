1e8dfbbb94a4ea295fc931b889378f4222e6fa9c
youwozai <18037553413@163.com>
2017-04-24 09:21:37 +0800

    Add files via upload

---
+++ txt/fa.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+	<title>Title</title>
+</head> 
+<body>
+<?php
+    #echo 'user: ' . $_POST["user"];
+    print_r($_POST);
+?>
+</body>
+</html>
